821157b5533e9d1f93a2dcf6685bcee266887c05
Jassi Bacha <klikster@gmail.com>
2023-10-20 17:46:38 -0700

    Got prisma schema and email auth working

---
+++ src/pages/index.tsx
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+function Home() {
+  const { data: sessionData } = useSession();
+  return (
+    <>
+      <Head>
+        <title>Full stack todo app</title>
+        <meta name="description" content="Full stack todo app" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#0f1235] to-[#090920]">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <div className="flex flex-col items-center gap-2">
+            <div className="flex flex-col items-center justify-center gap-4">
+              <p className="text-center text-l text-white">
+                {sessionData && <span>Logged in as {sessionData.user?.email}</span>}
+              </p>
+              <button
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                onClick={sessionData ? () => void signOut() : () => void signIn()}
+              >
+                {sessionData ? "Sign out" : "Sign in"}
+              </button>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default Home;
+
